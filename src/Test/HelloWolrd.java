0a014f405d21108a9543fd758fbe76219da1cd8f
MinhLong <longbo1208@gmail.com>
2024-04-07 21:24:06 +0700

    init project

---
+++ src/Test/HelloWolrd.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class HelloWolrd {
+	public static void main(String[] args) {
+		System.out.println("Hello Java AGAIN 3");
+		System.out.println("Hello Java AGAIN 3");
+		System.out.println("Hello Java AGAIN 3");
+		System.out.println("Hello Java AGAIN 3");
+
+	}
+}
